33d062be31fb3313d7b0a1211fed840165997896
Sahil <56181733+skassanjee@users.noreply.github.com>
2021-01-11 04:24:55 -0700

    more mongodb

--- models/tourModel.js
+++ models/tourModel.js
@@ -38,8 +38,26 @@ const tourSchema = new mongoose.Schema({
     },
     summary:{
         type: String,
+        trim: true,
+        required: [true, 'A tour must have a descriptiono']
+    },
+    description:{
+        type: String,
         trim: true
-    }
+    },
+    imageCover:{
+        type: String,
+        required: [true, 'a tour must have a cover image' ]
+    },
+
+    images: [String],
+
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
+
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
